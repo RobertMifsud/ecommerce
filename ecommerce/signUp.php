0c83646c308f470c43a0be76a687570242884961
RobertMifsud <rm1397@live.mdx.ac.uk>
2017-02-02 09:54:59 +0100

    Started CSS

--- ecommerce/signUp.php
+++ ecommerce/signUp.php
@@ -4,6 +4,7 @@
         <title>Sign Up</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -18,6 +19,7 @@
             <div class="col-sm-2">
             </div>
             <div class="col-sm-4">
+                <div class="formBorder">
                 <h2>Sign In</h1>
                     <form>
                         <div class="form-group">
@@ -25,11 +27,12 @@
                             <input type="text" class="form-control" id="userName"  placeholder="Enter User Name">        
                         </div>
                         <div class="form-group">
-                            <label for="userName">Password</label>
+                            <label for="password">Password</label>
                             <input type="password" class="form-control" id="userPassword" placeholder="Enter Password">
                         </div>
                         <input type="submit" class="btn btn-primary" id="submit-btn" value="Login" >
                     </form>
+                </div>
             </div>
             <div class="col-sm-4">
                 <h2>Register:</h1>
@@ -46,6 +49,15 @@
                             <label for="userEmail">Email address</label>
                             <input type="email" class="form-control" id="userEmail" aria-describedby="emailHelp" placeholder="Enter email">
                         </div>
+                         <div class="form-group">
+                            <label for="newuserName">New User Name</label>
+                            <input type="text" class="form-control" id="newuserName"  placeholder="Enter User Name">        
+                        </div>
+                        <div class="form-group">
+                            <label for="newpassword">New Password</label>
+                            <input type="newpassword" class="form-control" id="newuserPassword" placeholder="Enter Password">
+                        </div>
+                        
                         <input type="submit" class="btn btn-primary" id="submit-btn" value="Register" >
                     </form>
             </div>
